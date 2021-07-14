1d60c66eb4ed7d1b486a1029d7a00529015c134b
Freddie Mixell <fmixell@gmail.com>
2021-07-14 13:00:04 -0400

    use begin button to nagivate to quiz

--- src/components/HomeScreen/BeginBtn.js
+++ src/components/HomeScreen/BeginBtn.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function BeginBtn() {
     return (
-        <button className="button">
+        <Link
+            className="button"
+            to="/quiz"
+        >
             Begin
-        </button>
+        </Link>
     );
 }
