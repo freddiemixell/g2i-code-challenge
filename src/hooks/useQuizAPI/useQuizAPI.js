88bba6e5aedb23e287f32bc082f51c95abe27122
Freddie Mixell <fmixell@gmail.com>
2021-07-15 16:15:56 -0400

    return results

--- src/hooks/useQuizAPI/useQuizAPI.js
+++ src/hooks/useQuizAPI/useQuizAPI.js
@@ -1,31 +1,24 @@
-import { useState, useCallback } from 'react';
 import fetch from 'unfetch';
 
 const apiUrl = 'https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean';
 
 export default function useQuizAPI() {
-    let [ responseCode, setResponseCode ] = useState(null);
-    let [ results, setResults ] = useState([]);
-
-    let getResults = useCallback( async () => {
+    let getResults = async () => {
         try {
             let req = await fetch(apiUrl);
             let res = await req.json();
 
             if ( typeof res.results !== 'undefined' ) {
-                setResults( res.results );
-                setResponseCode( res.response_code );
+                return res.results;
             } else {
                 throw new Error( 'No results from quiz api fetch.' );
             }
         } catch ( error ) {
             console.log( error )
         }
-    }, [] );
+    };
 
     return {
-        responseCode,
-        results,
         getResults,
     }
 }
