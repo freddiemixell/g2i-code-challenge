37a13d76a62e6d98e1010c1bb31662f4315f8b0e
Freddie Mixell <fmixell@gmail.com>
2021-07-15 16:16:24 -0400

    using new dispatch system

--- src/components/QuizProvider/QuizProvider.js
+++ src/components/QuizProvider/QuizProvider.js
@@ -1,18 +1,26 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useEffect, useReducer } from 'react';
 
 import useQuizAPI from '../../hooks/useQuizAPI/useQuizAPI';
+import quizReducer, { initialState } from './QuizReducer';
 
 export const QuizContext = createContext({});
 
 export default function QuizProvider( { children } ) {
-    let { results, getResults } = useQuizAPI();
+    let { getResults } = useQuizAPI();
 
-    let [currentQuestion, setCurrentQuestion] = useState(0);
-    let [answers, setAnswers] = useState([]);
+    let [ state, dispatch ] = useReducer( quizReducer, initialState );
 
     useEffect( () => {
         try {
-            let q = async () => await getResults();
+            let q = async () => {
+                let results = await getResults();
+                dispatch( {
+                    type: 'UPDATE_QUESTIONS',
+                    payload: {
+                        questions: [ ...results ],
+                    },
+                } );
+            };
             q();
         } catch (error) {
             console.log(error)
@@ -25,22 +33,24 @@ export default function QuizProvider( { children } ) {
      * @returns true if theres more questions.
      */
     let nextQuestion = ( answer ) => {
+        dispatch({
+            type: 'ADD_ANSWER',
+            payload: { answer }
+        } );
 
-        let nextAnswers = [ ...answers ]
-        nextAnswers[ currentQuestion ] = answer;
-        setAnswers( nextAnswers );
+        dispatch( {
+            type: 'NEXT_QUESTION',
+        } );
 
-        setCurrentQuestion( currentQuestion + 1 );
-
-        return ( currentQuestion + 1 ) > 9 ? false : true;
+        return ( state.currentQuestion + 1 ) > 9 ? false : true;
     }
 
     return (
         <QuizContext.Provider
             value={ {
-                questions: results,
-                answers,
-                currentQuestion,
+                questions: state.questions,
+                answers: state.answers,
+                currentQuestion: state.currentQuestion,
                 nextQuestion,
             } }
         >
