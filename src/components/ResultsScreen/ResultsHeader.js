0b352e044b5136c40e926290d367beab79d62664
Freddie Mixell <fmixell@gmail.com>
2021-07-15 18:30:19 -0400

    tally score

--- src/components/ResultsScreen/ResultsHeader.js
+++ src/components/ResultsScreen/ResultsHeader.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+
+import {QuizContext} from '../QuizProvider/QuizProvider';
 
 export default function ResultsHeader() {
+    let { questions, answers } = useContext( QuizContext );
+    let [ correctAnswers, setCorrectAnswers ] = useState( 0 );
+
+    useEffect( () => {
+        let localCorrect = 0;
+        questions.forEach( ( { correct_answer }, index ) => {
+            let checkAnswer = correct_answer.toLowerCase() === answers[ index ];
+            if ( checkAnswer ) {
+                localCorrect++;
+            }
+        } );
+        setCorrectAnswers( localCorrect );
+    }, [] );
+
     return (
         <div className="container">
             <h1 className="title">
                 You scored
                 <br/>
-                3/10
+                {correctAnswers}/10
             </h1>
         </div>
     );
