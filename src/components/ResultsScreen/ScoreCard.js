9c4677822df7763be72e26005650ef90a2865df1
Freddie Mixell <fmixell@gmail.com>
2021-07-15 16:16:53 -0400

    mapped questions scored

--- src/components/ResultsScreen/ScoreCard.js
+++ src/components/ResultsScreen/ScoreCard.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import he from 'he';
 
 import ScoreCardItem from './ScoreCardItem';
+import {QuizContext} from '../QuizProvider/QuizProvider';
 
 export default function ScoreCard() {
+    let { answers, questions } = useContext( QuizContext );
+
+    let scoredItems = questions.map( ( { question, correct_answer }, index ) => {
+        let checkAnswer = correct_answer.toLowerCase() === answers[ index ];
+        return (
+            <ScoreCardItem
+                questionText={ he.decode( question ) }
+                isCorrect={ checkAnswer }
+            />
+        );
+    } );
+
     return (
         <div className="container">
-            <ScoreCardItem/>
-            <ScoreCardItem/>
-            <ScoreCardItem/>
-            <ScoreCardItem/>
+            { scoredItems }
         </div>
     );
 }
--- src/components/ResultsScreen/ScoreCardItem.js
+++ src/components/ResultsScreen/ScoreCardItem.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function ScoreCardItem() {
+export default function ScoreCardItem( { questionText = '', isCorrect = false } ) {
     return (
         <div class="scorecard__item">
-            <p>+ Unturned originally started as a Roblox game.</p>
+            <p>{ isCorrect ? '+' : '-' } { questionText }</p>
         </div>
     );
 }
